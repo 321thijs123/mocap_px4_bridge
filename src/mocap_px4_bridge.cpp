ba17ad06c9a0ee46ae01383d127b11cb4295810d
321thijs123 <thijs.hof@home.nl>
2022-10-28 14:46:01 +0200

    comply to px4_msgs overhaul

--- src/mocap_px4_bridge.cpp
+++ src/mocap_px4_bridge.cpp
@@ -1,12 +1,9 @@
-// #define PX4_ROS_TIMESYNC		// Enable to sync time between ROS and PX4
-
 #include <chrono>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 
-//#include <px4_msgs/msg/vehicle_mocap_odometry.hpp>
-#include <px4_msgs/msg/vehicle_visual_odometry.hpp>
+#include <px4_msgs/msg/vehicle_odometry.hpp>
 
 #ifdef PX4_ROS_TIMESYNC
 	#include <px4_msgs/msg/timesync.hpp>
@@ -24,38 +21,26 @@ class MocapPX4Bridge : public rclcpp::Node
 public:
 	MocapPX4Bridge() : Node("mocap_px4_bridge") {
 		poseSub = this->create_subscription<geometry_msgs::msg::PoseStamped>("/Robot_1/pose", 10, std::bind(&MocapPX4Bridge::poseCallback, this, _1));
-		//odomPub = this->create_publisher<px4_msgs::msg::VehicleMocapOdometry>("/fmu/vehicle_mocap_odometry/in", 10);
-		odomPub = this->create_publisher<px4_msgs::msg::VehicleVisualOdometry>("/fmu/vehicle_visual_odometry/in", 10);
-
-		#ifdef PX4_ROS_TIMESYNC
-			timesyncSub = this->create_subscription<px4_msgs::msg::Timesync>("fmu/timesync/out", 10, std::bind(&MocapPX4Bridge::timesyncCallback, this, _1));
-		#endif
+		odomPub = this->create_publisher<px4_msgs::msg::VehicleOdometry>("/fmu/vehicle_visual_odometry/in", 10);
 	}
 
 private:
 	void poseCallback(const geometry_msgs::msg::PoseStamped::UniquePtr);
 
 	rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr poseSub;
-	rclcpp::Publisher<px4_msgs::msg::VehicleVisualOdometry>::SharedPtr odomPub;
-
-	#ifdef PX4_ROS_TIMESYNC
-		rclcpp::Subscription<px4_msgs::msg::Timesync>::SharedPtr timesyncSub;
-		void timesyncCallback(const px4_msgs::msg::Timesync::UniquePtr);
-		int64_t timeoffset;   // Offset between ROS and PX4 clock.
-	#endif
+	rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr odomPub;
 };
 
 void MocapPX4Bridge::poseCallback(const geometry_msgs::msg::PoseStamped::UniquePtr poseMsg){
-	//px4_msgs::msg::VehicleMocapOdometry odomMsg;
-	px4_msgs::msg::VehicleVisualOdometry odomMsg;
+	px4_msgs::msg::VehicleOdometry odomMsg;
 
-	odomMsg.local_frame = odomMsg.LOCAL_FRAME_NED;
+	odomMsg.pose_frame = odomMsg.POSE_FRAME_NED;
 	odomMsg.timestamp = uint64_t(poseMsg->header.stamp.sec)*1000000 + uint64_t(poseMsg->header.stamp.nanosec)/1000;
 	odomMsg.timestamp_sample = odomMsg.timestamp;
 
-	odomMsg.x = poseMsg->pose.position.x;
-	odomMsg.y = -poseMsg->pose.position.y;
-	odomMsg.z = -poseMsg->pose.position.z;
+	odomMsg.position[0] = poseMsg->pose.position.x;
+	odomMsg.position[1] = -poseMsg->pose.position.y;
+	odomMsg.position[2] = -poseMsg->pose.position.z;
 
 	odomMsg.q[0] = std::nanf("0");
 
@@ -69,23 +54,12 @@ void MocapPX4Bridge::poseCallback(const geometry_msgs::msg::PoseStamped::UniqueP
 	// odomMsg.q[2] = poseMsg->pose.orientation.z;
 	// odomMsg.q[3] = poseMsg->pose.orientation.w;
 
-	std::cout << "X: " << odomMsg.x << '\t' <<
-				 "Y: " << odomMsg.y << '\t' <<
-				 "Z: " << odomMsg.z << "\n";
+	std::cout << "X: " << odomMsg.position[0] << '\t' <<
+				 "Y: " << odomMsg.position[1] << '\t' <<
+				 "Z: " << odomMsg.position[2] << "\n";
 
 	odomPub -> publish(odomMsg);
 }
-
-#ifdef PX4_ROS_TIMESYNC
-	void MocapPX4Bridge::timesyncCallback(const px4_msgs::msg::Timesync::UniquePtr msg){
-		const uint64_t ROSTime = now().nanoseconds()/1000;
-		const uint64_t PX4Time = msg->timestamp;
-		
-		timeoffset = PX4Time - ROSTime;
-
-		std::cout << "ROSTime: " << ROSTime << "\nPX4Time: " << PX4Time << "\nOffset:  " << timeoffset << "\n\n";
-	}
-#endif
 
 
 int main(int argc, char * argv[])
